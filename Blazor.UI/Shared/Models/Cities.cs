11acb4a434e11b568e49904a4c8853d3036d357a
alistairdevine <alistair.devine98@gmail.com>
2021-09-23 09:14:51 +0100

    Complete tutorial implementation

---
+++ Blazor.UI/Shared/Models/Cities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Blazor.Shared.Models
+{
+    public class Cities
+    {
+        [BsonId] [BsonRepresentation(BsonType.ObjectId)] 
+        public string Id { get; set; }
+        public string CityName { get; set; }
+    }
+}
---
+++ Blazor.UI/Shared/Models/EmployeeDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Shared.Models
+{
+    public class EmployeeDbContext
+    {
+        private readonly IMongoDatabase _mongoDatabase; 
+        public EmployeeDbContext() 
+        { 
+            var client = new MongoClient("mongodb://localhost:27017"); 
+            _mongoDatabase = client.GetDatabase("EmployeeDB"); 
+        }
+        public IMongoCollection<Employee> EmployeeRecord 
+        { 
+            get { return _mongoDatabase.GetCollection<Employee>("EmployeeRecord"); } 
+        }
+        public IMongoCollection<Cities> CityRecord 
+        { 
+            get { return _mongoDatabase.GetCollection<Cities>("Cities"); } 
+        }
+    }
+}
